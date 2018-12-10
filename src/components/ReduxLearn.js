8c92553ca8aff609eb3c1733898885086ca98f6e
Ihor Duchenko <igordu4enko@ukr.net>
2018-12-10 15:52:18 +0200

    Finish Redux

--- src/components/ReduxLearn.js
+++ src/components/ReduxLearn.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import store from "../store/index";
 import { addArticle } from "../actions/index";
 
+import List from './List';
+import Form from './Form';
+
 class ReduxLearn extends Component{
   render() {
     return (
-    	<div className="container">
-				<div>Redux</div>
+    	<div className="container my-5">
+    		<div className="row">
+					<div className="col-md-6">
+            <h2>Add new article</h2>
+						<Form />
+					</div>
+          <div className="col-md-6">
+            <h2>Articles</h2>
+            <List />
+          </div>
+				</div>
     	</div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import store from './store';
 
 import Header from './components/Header';
 import Main from './components/Main';
@@ -13,19 +15,21 @@ import ReduxLearn from "./components/ReduxLearn";
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <Header/>
-        <main>
-          <Switch>
-            <Route exact path={'/'} component={Main}/>
-            <Route exact path={'/posts'} component={Posts}/>
-            <Route exact path={'/posts/:slug'} component={PostSingle}/>
-            <Route exact path={'/play-js'} component={PlayJs}/>
-            <Route exact path={'/articles'} component={Articles}/>
-            <Route exact path={'/redux-learn'} component={ReduxLearn}/>
-          </Switch>
-        </main>
-      </React.Fragment>
+      <Provider store={store}>
+        <React.Fragment>
+          <Header/>
+          <main>
+            <Switch>
+              <Route exact path={'/'} component={Main}/>
+              <Route exact path={'/posts'} component={Posts}/>
+              <Route exact path={'/posts/:slug'} component={PostSingle}/>
+              <Route exact path={'/play-js'} component={PlayJs}/>
+              <Route exact path={'/articles'} component={Articles}/>
+              <Route exact path={'/redux-learn'} component={ReduxLearn}/>
+            </Switch>
+          </main>
+        </React.Fragment>
+      </Provider>
     );
   }
 }
